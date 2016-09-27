1465ff7efc3f9cb56c6f273a680933a101e9d565
needmorecowbell <amusciano@gmail.com>
2016-09-27 01:15:15 -0400

    First commit

---
+++ js/submitSymptons.js
@@ -0,0 +1,13 @@
+  $('#btnSubmit').on('click', function (e) {
+        console.log("Entered")
+        var x=[document.getElementById("txtSym1").value(),document.getElementById("txtSym2").value(),document.getElementById("txtSym3").value(),
+        document.getElementById("txtSym4").value(),document.getElementById("txtSym5").value()]
+    
+        console.log(x[0])
+     //your awesome code here
+    
+    });
+
+
+
+
